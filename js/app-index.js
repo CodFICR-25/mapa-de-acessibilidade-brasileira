d35e5ca3a03a380ff2c957ee2c8eecc1a2b8b759
Ingrydt Alcântara <ingrydt alcantara>
2020-06-12 19:45:03 -0300

    junção a aba configurações

--- js/app-index.js
+++ js/app-index.js
@@ -1,10 +1,19 @@
 
-function carregaConfiguracoes(){
+function carregaConfiguracoes() {
+    /*escondendo sub aba dos locais*/
     document.getElementsByClassName("subtab")[0].style.display = "none";
     document.getElementsByClassName("subtabhoteis")[0].style.display = "none";
     document.getElementsByClassName("subtabrest")[0].style.display = "none";
     document.getElementsByClassName("subtabshop")[0].style.display = "none";
     document.getElementsByClassName("subtabpontos")[0].style.display = "none";
+
+    //inicio da aba config
+
+    document.getElementById("Configuracoes").style.display = "none";
+    document.getElementsByClassName("subtabfav")[0].style.display = "none";
+    document.getElementsByClassName("subtabrev")[0].style.display = "none";
+    document.getElementsByClassName("subtabava")[0].style.display = "none";
+    document.getElementsByClassName("subtabseuspontos")[0].style.display = "none";
 }
 function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
@@ -18,45 +27,76 @@ function openCity(evt, cityName) {
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-    
+
     /*Aba principal*/
-    if(evt.currentTarget.id == 'btnLocais'){
+    if (evt.currentTarget.id == 'btnLocais') {
         document.getElementsByClassName("subtab")[0].style.display = "block";
         document.getElementsByClassName("subtabhoteis")[0].style.display = "block";
         document.getElementsByClassName("subtabrest")[0].style.display = "none";
         document.getElementsByClassName("subtabshop")[0].style.display = "none";
         document.getElementsByClassName("subtabpontos")[0].style.display = "none";
-    }else if(evt.currentTarget.id == 'btnMapa' || evt.currentTarget.id == 'btnProfile' || evt.currentTarget.id == 'btnConfiguracoes'){
+        document.getElementById("Configuracoes").style.display = "none";
+    } else if (evt.currentTarget.id == 'btnMapa' || evt.currentTarget.id == 'btnProfile' || evt.currentTarget.id == 'btnConfiguracoes') {
+        document.getElementById("Configuracoes").style.display = "none";
         document.getElementsByClassName("subtab")[0].style.display = "none";
         document.getElementsByClassName("subtabhoteis")[0].style.display = "none";
         document.getElementsByClassName("subtabrest")[0].style.display = "none";
         document.getElementsByClassName("subtabshop")[0].style.display = "none";
         document.getElementsByClassName("subtabpontos")[0].style.display = "none";
     }
-    /*SubAbaLocais*/  
-    if(evt.currentTarget.id == 'btnHoteis'){
+
+    if (evt.currentTarget.id == 'btnConfiguracoes') {
+        document.getElementById("Configuracoes").style.display = "block";
+    }
+
+    /*SubAbaLocais*/
+    if (evt.currentTarget.id == 'btnHoteis') {
         document.getElementsByClassName("subtabhoteis")[0].style.display = "block";
         document.getElementsByClassName("subtabrest")[0].style.display = "none";
         document.getElementsByClassName("subtabshop")[0].style.display = "none";
         document.getElementsByClassName("subtabpontos")[0].style.display = "none";
-    }else if (evt.currentTarget.id == 'btnRestaurante'){
+    } else if (evt.currentTarget.id == 'btnRestaurante') {
         document.getElementsByClassName("subtabhoteis")[0].style.display = "none";
         document.getElementsByClassName("subtabrest")[0].style.display = "block";
         document.getElementsByClassName("subtabshop")[0].style.display = "none";
         document.getElementsByClassName("subtabpontos")[0].style.display = "none";
-    }else if (evt.currentTarget.id == 'btnShopping'){
+    } else if (evt.currentTarget.id == 'btnShopping') {
         document.getElementsByClassName("subtabhoteis")[0].style.display = "none";
         document.getElementsByClassName("subtabrest")[0].style.display = "none";
         document.getElementsByClassName("subtabshop")[0].style.display = "block";
         document.getElementsByClassName("subtabpontos")[0].style.display = "none";
-    }else if (evt.currentTarget.id == 'btnPontos-Turisticos'){
+    } else if (evt.currentTarget.id == 'btnPontos-Turisticos') {
         document.getElementsByClassName("subtabhoteis")[0].style.display = "none";
         document.getElementsByClassName("subtabrest")[0].style.display = "none";
         document.getElementsByClassName("subtabshop")[0].style.display = "none";
         document.getElementsByClassName("subtabpontos")[0].style.display = "block";
     }
-
-
-    
+    // sub abas configuracoes
+    if (evt.currentTarget.id == 'btnFavoritos') {
+        document.getElementsByClassName("subtabfav")[0].style.display = "block";
+        document.getElementsByClassName("subtabrev")[0].style.display = "none";
+        document.getElementsByClassName("subtabava")[0].style.display = "none";
+        document.getElementsByClassName("subtabseuspontos")[0].style.display = "none";
+    } else if (evt.currentTarget.id == 'btnReviews') {
+        document.getElementsByClassName("subtabfav")[0].style.display = "none";
+        document.getElementsByClassName("subtabrev")[0].style.display = "block";
+        document.getElementsByClassName("subtabava")[0].style.display = "none";
+        document.getElementsByClassName("subtabseuspontos")[0].style.display = "none";
+    } else if (evt.currentTarget.id == 'btnAvaliacoes') {
+        document.getElementsByClassName("subtabfav")[0].style.display = "none";
+        document.getElementsByClassName("subtabrev")[0].style.display = "none";
+        document.getElementsByClassName("subtabava")[0].style.display = "block";
+        document.getElementsByClassName("subtabseuspontos")[0].style.display = "none";
+    } else if (evt.currentTarget.id == 'btnPontos-Cadastrados') {
+        document.getElementsByClassName("subtabfav")[0].style.display = "none";
+        document.getElementsByClassName("subtabrev")[0].style.display = "none";
+        document.getElementsByClassName("subtabava")[0].style.display = "none";
+        document.getElementsByClassName("subtabseuspontos")[0].style.display = "block";
+    } else {
+        document.getElementsByClassName("subtabfav")[0].style.display = "none";
+        document.getElementsByClassName("subtabrev")[0].style.display = "none";
+        document.getElementsByClassName("subtabava")[0].style.display = "none";
+        document.getElementsByClassName("subtabseuspontos")[0].style.display = "none";
+    }
 }
 
